ec06706878bfcefe5dbcf0c87e55ad72cd31d034
rickywid <rickywid@hotmail.com>
2020-05-15 16:10:10 -0400

    create signup page UI

--- pages/signup/index.tsx
+++ pages/signup/index.tsx
@@ -1,23 +1,173 @@
 import React, { useEffect } from 'react';
 import Head from 'next/head';
+import './signup.less';
+import {
+  Form,
+  Input,
+  Tooltip,
+  Checkbox,
+  Button,
+} from 'antd';
+import { QuestionCircleOutlined } from '@ant-design/icons';
 import Layout from '../../components/layout';
-import styles from './signup.module.css';
+
+const formItemLayout = {
+  labelCol: {
+    xs: {
+      span: 24,
+    },
+    sm: {
+      span: 8,
+    },
+  },
+  wrapperCol: {
+    xs: {
+      span: 24,
+    },
+    sm: {
+      span: 16,
+    },
+  },
+};
+const tailFormItemLayout = {
+  wrapperCol: {
+    xs: {
+      span: 24,
+      offset: 0,
+    },
+    sm: {
+      span: 16,
+      offset: 8,
+    },
+  },
+};
 
 const SignUp:React.FunctionComponent = () => {
+  const [form] = Form.useForm();
+
+  const onFinish = (values) => {
+    console.log('Received values of form: ', values);
+  };
+
   useEffect(() => {
     console.log('signup mounted');
   });
 
   return (
     <Layout>
-      <div className={styles.container}>
-        <Head>
-          <title>SignUp</title>
-          <link rel="icon" href="/favicon.ico" />
-        </Head>
-        <main>
-          Signup Page
-        </main>
+      <div className="container">
+        <div className="content">
+          <Head>
+            <title>Sign up</title>
+            <link rel="icon" href="/favicon.ico" />
+          </Head>
+          <strong className="signup-title">Sign up</strong>
+          <Form
+            form={form}
+            name="register"
+            onFinish={onFinish}
+            layout="vertical"
+            scrollToFirstError
+          >
+            <Form.Item
+              name="email"
+              label="E-mail"
+              rules={[
+                {
+                  type: 'email',
+                  message: 'The input is not valid E-mail!',
+                },
+                {
+                  required: true,
+                  message: 'Please input your E-mail!',
+                },
+              ]}
+            >
+              <Input />
+            </Form.Item>
+
+            <Form.Item
+              name="password"
+              label="Password"
+              rules={[
+                {
+                  required: true,
+                  message: 'Please input your password!',
+                },
+              ]}
+              hasFeedback
+            >
+              <Input.Password />
+            </Form.Item>
+
+            <Form.Item
+              name="confirm"
+              label="Confirm Password"
+              dependencies={['password']}
+              hasFeedback
+              rules={[
+                {
+                  required: true,
+                  message: 'Please confirm your password!',
+                },
+                ({ getFieldValue }) => ({
+                  validator(rule, value) {
+                    if (!value || getFieldValue('password') === value) {
+                      return Promise.resolve();
+                    }
+
+                    return Promise.reject('The two passwords that you entered do not match!');
+                  },
+                }),
+              ]}
+            >
+              <Input.Password />
+            </Form.Item>
+
+            <Form.Item
+              name="nickname"
+              label={
+                (
+                  <span>
+                    Nickname&nbsp;
+                    <Tooltip title="What do you want others to call you?">
+                      <QuestionCircleOutlined />
+                    </Tooltip>
+                  </span>
+                  )
+              }
+              rules={[
+                {
+                  required: true,
+                  message: 'Please input your nickname!',
+                  whitespace: true,
+                },
+              ]}
+            >
+              <Input />
+            </Form.Item>
+
+            <Form.Item
+              name="agreement"
+              valuePropName="checked"
+              rules={[
+                {
+                  validator: (_, value) => (value ? Promise.resolve() : Promise.reject('Should accept agreement')),
+                },
+              ]}
+            >
+              <Checkbox>
+                I have read and understand the
+                <a href="/terms"> rules</a>
+              </Checkbox>
+            </Form.Item>
+            <Form.Item>
+              <Button type="primary" htmlType="submit" className="form-signup-button">
+                Sign up
+              </Button>
+            </Form.Item>
+          </Form>
+        </div>
       </div>
     </Layout>
   );
