6c74805b7e43ed5a017d15a5a26d6ab7e8fe68c5
rickywid <rickywid@hotmail.com>
2020-05-17 19:29:17 -0400

    create front page projects UI; replace airbnb-eslint w/ react-app

---
+++ components/projects-main.tsx
@@ -0,0 +1,47 @@
+import React, { useEffect } from 'react';
+import { NextPage } from 'next'
+import { IProject } from '../mocks/data';
+import './styles/projects-main.less';
+
+interface IProps {
+  children?: React.ReactNode,
+  projects: IProject[]
+}
+
+const ProjectsMain:NextPage<IProps> = ({ projects }) => {
+  useEffect(() => {
+    console.log('projects main mounted');
+    
+  });
+  console.log(projects)
+  const renderProjects = () => {
+    return projects.map(project => (
+      <div className="project-card" key={project.id}>
+        <div className="project-card-left">
+          <img className="img" src={project.thumbnail} alt=""/>
+          <div className="project-detail">
+            <strong>{project.name}</strong>
+            <p>{project.descriptionShort}</p>
+            <small>Comments: {project.commentCount}</small>
+            {project.tags.map(tag => <small>{tag}</small>)}
+            <a href={project.url}>website</a>
+          </div>
+        </div>
+        <div className="project-card-right">
+         <p>{project.upvoteCount}</p>
+        </div>
+      </div>
+    ))
+  }
+
+  return (
+    <div>
+      test
+      {renderProjects()}
+    </div>
+  );
+};
+
+export default ProjectsMain;
+
+
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,20 +1,35 @@
 import React from 'react';
+import { NextPage, GetServerSideProps } from 'next';
 import Head from 'next/head';
 import BaseLayout from '../components/layout';
+import ProjectsMain from '../components/projects-main';
 import styles from './index.module.less';
+import data, { IProject } from '../mocks/data';
 
-const Home:React.FunctionComponent = () => (
+const Home: NextPage = ({projects}:{children?: React.ReactNode, projects: IProject[]}) => (
   <BaseLayout>
     <div className={styles.container}>
       <Head>
         <title>My awesome app</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main>
-        body
-      </main>
+      <ProjectsMain projects={projects} />
     </div>
+      <style global jsx>{`
+        body {
+          border: 5px solid black;
+        }
+      `}</style>
   </BaseLayout>
 );
 
+export const getServerSideProps: GetServerSideProps<{projects: IProject[]}> = async context => {
+
+  // db queries
+  return {
+    props: { projects: data.projects }
+  }
+}
+
 export default Home;
+
